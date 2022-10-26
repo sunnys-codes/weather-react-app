50829a83a90f03addf3afb64994b1da72e65bb7d
sunnywithasmile <112489480+sunnywithasmile@users.noreply.github.com>
2022-10-26 15:29:53 -0400

    Added the github repo url

--- src/App.js
+++ src/App.js
@@ -6,6 +6,14 @@ import './App.css';
 
 
 export default function App() {
-  return <div className="App"> Weather </div>;
+  return <div className="App"> 
+   <h1>Weather App</h1>
+   <footer>
+    This project was coded by Sandra Thermildor and is open sourced on github
+  <a href="https://keen-salamander-0d680a.netlify.app" target="_blank">
+  </a>
+  </footer>
+  </div>;
+
 }
 
