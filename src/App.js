a7c8b3d2aa8129af8ef82c841feb7ddcb2408253
sunnywithasmile <112489480+sunnywithasmile@users.noreply.github.com>
2022-10-26 15:39:45 -0400

    Deleted weather.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 
 import React from "react";
-import Weather from "./Weather";
+
 
 export default function App() {
   return <div className="App"> 
    <h1>Weather App</h1>
    <footer>
     This project was coded by Sandra Thermildor and is open sourced on github
-  <a href="https://keen-salamander-0d680a.netlify.app" target="_blank">
+  <a href="https://keen-salamander-0d680a.netlify.app" target="_blank" rel="noreferrer">
   </a>
   </footer>
   </div>;
