17e6a6f8667e3703b8a9f4ae3bcc6139ea4ee21c
sunnywithasmile <112489480+sunnywithasmile@users.noreply.github.com>
2022-10-26 15:18:08 -0400

    Cleaned up documents

--- src/App.js
+++ src/App.js
@@ -4,15 +4,8 @@ import Weather from "./Weather";
 
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Hello from React</h1>
-        <Weather city="Paris"/>
-      </header>
-    </div>
-  );
+
+export default function App() {
+  return <div className="App"> Weather </div>;
 }
 
-export default App;
